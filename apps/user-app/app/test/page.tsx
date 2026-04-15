4fd58493c5ead7cc400e5c3a409cd2a626c4ba15
Anurag <akajeet2004i@gmail.com>
2026-04-15 06:06:13 +0530

    lkh

--- apps/user-app/app/test/page.tsx
+++ apps/user-app/app/test/page.tsx
@@ -1,7 +1,10 @@
 "use client"
-import {useBalance} from "@repo/store/useBalance"
-export default function() {
+
+
+import { useBalance } from "@repo/store/useBalance"
+export default function () {
   const balance = useBalance();
   return <div>
     hi there {balance}
-  </div>} 
+  </div>
+} 
