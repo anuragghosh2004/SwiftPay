35c2991ff7cf3ca44545bb09bf3193fe4517c30c
Anurag <akajeet2004i@gmail.com>
2026-04-15 06:51:02 +0530

    heohh

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,19 +1,21 @@
-export  * from "@prisma/adapter-pg";
+export * from "@prisma/adapter-pg";
 export * from "@prisma/client";
 export * from "pg"
 
- import { Pool } from "pg";
+
+
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import * as dotenv from "dotenv";   
+import * as dotenv from "dotenv";
 dotenv.config({ path: "../../.env" });
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is missing! Make sure your .env file is loaded.");
 }
 // Initialize everything INSIDE the package
 const pool = new Pool({
-    // Make sure your root .env file has this exact variable
-    connectionString: process.env.DATABASE_URL
+  // Make sure your root .env file has this exact variable
+  connectionString: process.env.DATABASE_URL
 });
 
 const adapter = new PrismaPg(pool as any);
